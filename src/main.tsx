3535add63a04a092ed6260f47872d289c45e355f
nikhil <83238692+tellrajnikhil@users.noreply.github.com>
2026-09-24 22:37:31 +0530

    Add Google Analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,21 @@ import "./styles/tokens.css";
 import "./styles/global.css";
 import App from "./App.tsx";
 
+const GA_ID = "G-Y4SHLFTM2H";
+
+const script = document.createElement("script");
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+document.head.appendChild(script);
+
+(window as any).dataLayer = (window as any).dataLayer || [];
+(window as any).gtag = function (...args: unknown[]) {
+  (window as any).dataLayer.push(args);
+};
+
+(window as any).gtag("js", new Date());
+(window as any).gtag("config", GA_ID);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
